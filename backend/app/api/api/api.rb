d076114ec4ccf878a275fab1c59e8ab6acfa1b6f
tom <levanchi74@gmail.com>
2018-07-23 11:07:49 +0700

    modified helpers auth

--- backend/app/api/api/api.rb
+++ backend/app/api/api/api.rb
@@ -16,18 +16,37 @@ module Api
 		end
 		#rescue_from :all
 		
-		helpers do
-      def authorize_user!
-        error!("Token Invalid", 400) unless headers["Access-Token"].present?
-        error!("Unauthorize", unauthorized) unless current_user
+		# helpers do
+    #   def authorize_user!
+    #     error!("Token Invalid", 400) unless headers["Access-Token"].present?
+    #     error!("Unauthorize", unauthorized) unless current_user
+    #   end
+    #   def current_user
+    #     @current_user ||= User.find_by(confirmation_token: headers["Access-Token"])
+    #   end
+    # end
+    helpers do 
+      def authenticate!
+        error!('Unauthorized. Invalid or expired token.', 401) unless current_user
       end
+
       def current_user
-        @current_user ||= User.find_by(confirmation_token: headers["Access-Token"])
+        # find token. Check if valid.
+        token = ApiKey.where(access_token: params[:token]).first
+        if token && !token.expired?
+          @current_user = User.find(token.user_id)
+        else
+          false
+        end
+
       end
-		end
+    end
+
+
     
     
     mount V1::AuthApi
+    
 
 		# before do
     #   authorize_user!
