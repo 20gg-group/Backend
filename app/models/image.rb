eae05b9b40695e071aa719b7759bd1fea245c742
tom <levanchi74@gmail.com>
2018-07-26 23:22:18 +0700

    upload image with papaerclip to s3

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,9 +1,10 @@
 class Image < ApplicationRecord
 	belongs_to :post , required: false
 
-	# validates :extra_image, attachment_presence: true
-	
-  has_attached_file :image, styles: { medium: "400x400>", thumb: "200x200>" }, default_url: "/images/error_picture.png"
-  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+  has_attached_file :avatar, styles: {
+    thumb: '100x100>',
+    square: '200x200#',
+    medium: '300x300>'
+  }  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
   validates_with AttachmentSizeValidator, attributes: :image, less_than: 1.megabytes  
 end
