cd6376ab0dd9aa9d2b4f867133b5b4fe2cfd826a
tom <levanchi74@gmail.com>
2018-07-26 23:36:15 +0700

    ko nan

--- app/models/image.rb
+++ app/models/image.rb
@@ -5,6 +5,6 @@ class Image < ApplicationRecord
     thumb: '100x100>',
     square: '200x200#',
     medium: '300x300>'
-  }  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
-  validates_with AttachmentSizeValidator, attributes: :image, less_than: 1.megabytes  
+  } 
+   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
