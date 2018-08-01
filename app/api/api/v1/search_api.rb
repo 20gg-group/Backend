50f5ba8066a80f86d59d0760f67c9b88c17b0a23
tom <levanchi74@gmail.com>
2018-08-01 14:46:21 +0700

    fix search api

--- app/api/api/v1/search_api.rb
+++ app/api/api/v1/search_api.rb
@@ -5,11 +5,19 @@ module Api::V1
         def search_post 
           @p = []
           @result = []
-          @id.each do |y|          
-            @p.push(Post.where("posts.id = ? AND posts.price <= ? AND posts.price >= ? AND posts.type_house = ?",y ,params[:max_price] ,params[:min_price] ,params[:type_house]))           
+          if params[:max_price] && params[:min_price] && params[:type_house]
+            @id.each do |y|          
+              @p.push(Post.where("posts.id = ? AND posts.price <= ? AND posts.price >= ? AND posts.type_house = ?",y ,params[:max_price] ,params[:min_price] ,params[:type_house]))           
+            end 
+            @p.reject { |c| c.empty? }
           end 
-          @p.reject { |c| c.empty? }
-        end
+          if params[:max_price] && params[:min_price] 
+            @id.each do |y|          
+              @p.push(Post.where("posts.id = ? AND posts.price <= ? AND posts.price >= ?",y ,params[:max_price] ,params[:min_price]))           
+            end 
+            @p.reject { |c| c.empty? }
+          end 
+        end       
       end    
       # method GET
       namespace :search_multi do       
@@ -21,12 +29,37 @@ module Api::V1
           optional :type_house,   type: Integer           
         end
         get do
-          @id = []        
-          arr = Address.where(city: params[:city], district: params[:district])
-          arr.each do |i|            
-            @id.push(i.post_id)           
-          end   
-          search_post                 
+          if params[:city] && params[:district]
+            @id = []        
+            arr = Address.where(city: params[:city], district: params[:district])
+            arr.each do |i|            
+              @id.push(i.post_id)           
+            end   
+            search_post
+          end         
+          if params[:city]
+            @id = []        
+            arr = Address.where(city: params[:city])
+            arr.each do |i|            
+              @id.push(i.post_id)           
+            end   
+            search_post
+          end          
+          if params[:district]
+            @id = []        
+            arr = Address.where(district: params[:district])
+            arr.each do |i|            
+              @id.push(i.post_id)           
+            end   
+            search_post
+          else 
+            @id = []        
+            arr = Address.all
+            arr.each do |i|            
+              @id.push(i.post_id)           
+            end   
+            search_post  
+          end                     
         end
       end
       # get posts  - type house
@@ -34,8 +67,8 @@ module Api::V1
         params do          
           optional :type_house, type: Integer           
         end
-        get do
-          Post.where("posts.type_house = ? " ,params[:type_house])               
+        get do         
+          Post.where("posts.type_house = ? " ,params[:type_house])                     
         end
       end
 
