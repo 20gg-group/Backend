4c869c44cf1e3948ceda27ad765f76a5ae9c350c
tom <levanchi74@gmail.com>
2018-08-02 14:12:18 +0700

    test git

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-  
-root 'welcome#welcome_page'
+  #root 'welcome#welcome_page'
   namespace :admin do
     root to: 'sessions#new'
     get '/login', to: 'sessions#new'
@@ -9,16 +7,8 @@ root 'welcome#welcome_page'
     delete '/', to: 'sessions#destroy'
     resources :users
     #devise_for :users
-    resources :posts
+    #resources :posts
   end
-=======
-
-
-	resources :users
-	resources :posts
-	#get 'posts#index'
-  #devise_for :users
->>>>>>> tincms
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   mount Api::Api =>"/"
   mount GrapeSwaggerRails::Engine => '/swagger'
