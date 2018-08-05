0fea112c4a288639a3877206c1f436da5f5acd83
tom <levanchi74@gmail.com>
2018-08-05 21:18:43 +0700

    fix change password = add column reset password token

--- db/migrate/05_devise_create_users.rb
+++ db/migrate/05_devise_create_users.rb
@@ -5,6 +5,10 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
       
+      ## Recoverable
+      t.string   :reset_password_token
+      t.datetime :reset_password_sent_at
+
       t.string :full_name
       t.string :phone_number
       t.attachment :avatar
